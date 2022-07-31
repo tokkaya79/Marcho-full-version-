62423b3f0355a9460dc31c3db8e658f861558fb0
tokkaya79 <tokkaya79@gmail.com>
2022-07-31 22:29:10 +0300

    added product-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,33 @@
 $(function(){
 
+ $('.product-tabs__top-item').on('click', function (e) {
+  e.preventDefault();
+  $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+  $(this).addClass('product-tabs__top-item--active');
+  $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+  $($(this).attr('href')).addClass('product-tabs__content-item--active');
+
+ });
+
+
+
+ $('.product-slide__thumb').slick({
+  asNavFor: '.product-slide__big',
+  focusOnSelect: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  vertical: true,
+  draggable: false,
+  arrows: false
+ });
+
+ $('.product-slide__big').slick({
+  asNavFor: '.product-slide__thumb',
+  draggable: false,
+  arrows: false,
+  fade: true
+ });
+
 $('.shop-content__filter-btn').on('click', function () {
  $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
  $(this).addClass('shop-content__filter-btn--active');
@@ -16,8 +44,7 @@ $('.button-list').on('click', function () {
 
  });
 
- $('.select-style').styler();
-
+ $('.select-style, .product-item__item-num').styler();
 
 
  $('.filter-price__input').ionRangeSlider({
@@ -42,6 +69,8 @@ $('.button-list').on('click', function () {
     autoplay: true,
     autoplaySpead: 2000,
   });
+
+
 
  $(".star").rateYo({
 
